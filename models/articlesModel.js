9a4d34a485970ff36de2d0e4174e7f1fe75f39dc
Mar <mar@Berrys-MacBook-Air.local>
2024-02-29 11:06:47 +0100

    fix article push

--- models/articlesModel.js
+++ models/articlesModel.js
@@ -11,29 +11,29 @@ const articleSchema = new mongoose.Schema({
   },
   sous_titre: {
     type: String,
-    required: [true, "Rentrez un sous titre"],
+    // required: [true, "Rentrez un sous titre"],
+
   },
   thumbanails: {
     type: String,
     required: [true, "Une image de mise en avant et requise"],
   },
 
-
   contenu: {
     type: String,
     required: [true, "Rentrez le contenu de l'article"],
   },
 
-  
+
   type: {
     type: Number,
     default: 2, 
   },
   photos: {
     type: [String], 
-    required: [true, "Au moins une photo est requise"],
+    // required: [true, "Au moins une photo est requise"],
   },
-  
+ 
   agent: { type: mongoose.Schema.Types.ObjectId, ref: "Account" },
 });
 
